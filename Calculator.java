5eb90f0ab1480c0e09dec90969cd4e1cc61823e7
bernard <bernard.adarkwah@meltwater.org>
2015-09-18 22:05:58 +0000

    Modify the average square root and main methods.

--- Calculator.java
+++ Calculator.java
@@ -8,12 +8,9 @@
 
 			Scanner inp = new Scanner(System.in);
 
-			System.out.println("Enter first number");
-			double one = inp.nextDouble();
-			double two;
-
-
-			System.out.println("Enter The operator ?");
+			System.out.println("WELCOME TO MY CALCULATOR");
+			System.out.println("What operation do you want to perform?");
+			
 			System.out.println("1>>>ADD");
 			System.out.println("2>>>MULTIPLY");
 			System.out.println("3>>>SUBTRACT");
@@ -26,20 +23,35 @@
 
 			int sign = inp.nextInt();
 			while(sign > 8 || sign < 1){
-				System.out.print("Please reenter operator number 1-8: ");
+				System.out.print("Please re-enter operator number 1-8: ");
 				sign = inp.nextInt();
 			}
 
 			if (sign == 7){
-				int chi = (int) one;
+				System.out.println("Enter your number");
+				int chi = inp.nextInt();
 				squareRoot(chi);
 				
-			}
+			}else if (sign == 6){
+					System.out.println("Enter First number");
+					int num1=inp.nextInt();
+					System.out.println("Enter First number");
+					int num2=inp.nextInt();
+				 exponent(num1 , num2 );
+			}else if (sign == 8){ 
+					System.out.println("Enter First number");
+					int num1 = inp.nextInt();
+					System.out.println("Enter First number");
+					int num2 = inp.nextInt();
+					average(num1 , num2 );
+				}
 
 			else{
 
+				System.out.println("Enter First number");
+				double one = inp.nextDouble();
 				System.out.println("Enter Second number");
-				two = inp.nextDouble();
+				double two = inp.nextDouble();
 
 				if (sign == 1){ 
 					add(one , two );
@@ -51,12 +63,6 @@
 				 divide(one , two );
 				}else if (sign == 5){ 
 					modulo(one , two );
-				}else if (sign == 6){
-				 exponent(one , two );
-				}else if (sign == 8){ 
-					int num1 = (int) one;
-					int num2 = (int) two;
-					average(num1 , num2 );
 				}else{ 
 					System.out.println("Check the operation and try again");
 				}
@@ -99,8 +105,8 @@
 		
 
 
-		public static void exponent(double one, double two){
-			double result = 1;
+		public static void exponent(int one, int two){
+			int result = 1;
 			for(int i = 0 ; i< two ; i++){
 				result *= one;
 			}
@@ -122,8 +128,9 @@
 		}
 
 		public static void average(int a, int b){
+			float answer = (float)a+(((float)b-(float)a)/2) ;
 
-			System.out.println("The average of the numbers is " + ((float)a+(float)(b-a)/2));
+			System.out.println("The average of the numbers is " + answer);
 		}
 
 
